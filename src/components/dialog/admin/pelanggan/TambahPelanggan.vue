5d334939069fa72b7f120f6f5825e1ad3215cf6e
ardanwungkul <ardanwungkul14@gmail.com>
2025-07-21 15:07:28 +0700

    pelanggan

---
+++ src/components/dialog/admin/pelanggan/TambahPelanggan.vue
@@ -0,0 +1,98 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+    method: Function,
+})
+
+const form = ref({
+    nameserver1: null,
+    nameserver2: null,
+    tanggal_ns: null,
+})
+async function submit() {
+    try {
+        await props.method(form.value)
+        form.value.nameserver1 = null
+        form.value.nameserver2 = null
+        form.value.tanggal_ns = null
+    } catch (error) {}
+}
+</script>
+<template>
+    <v-dialog class="max-w-xl" :attach="null">
+        <template v-slot:activator="{ props: activatorProps }">
+            <button
+                v-bind="activatorProps"
+                class="!bg-secondary-3 text-white !text-sm px-5 py-2 rounded-lg hover:!bg-secondary-3/80 transition-all duration-300"
+            >
+                Tambah Pelanggan
+            </button>
+        </template>
+
+        <template v-slot:default="{ isActive }">
+            <div
+                class="bg-white dark:!bg-dark-primary-1 !p-5 border dark:!border-white rounded-lg w-full"
+            >
+                <div
+                    class="flex border-b dark:!border-typography-2 items-center justify-between px-3 pt-1 !pb-3"
+                >
+                    <p class="text-lg dark:!text-white">Tambah Pelanggan</p>
+                    <button @click="isActive.value = false">
+                        <i class="fa-solid fa-x dark:!text-white"></i>
+                    </button>
+                </div>
+                <div class="!px-3 !pt-3">
+                    <form @submit.prevent="submit">
+                        <div class="!space-y-3">
+                            <div>
+                                <label class="block mb-1 text-start dark:!text-white text-sm"
+                                    >Nameserver 1</label
+                                >
+                                <input
+                                    type="text"
+                                    v-model="form.nameserver1"
+                                    placeholder="Masukkan Nameserver 1"
+                                    required
+                                    class="w-full !p-2.5 border dark:!border-typography-3 dark:!bg-dark-primary-2 rounded-lg dark:!text-white !text-sm"
+                                />
+                            </div>
+                            <div>
+                                <label class="block mb-1 text-start dark:!text-white text-sm"
+                                    >Nameserver 2</label
+                                >
+                                <input
+                                    type="text"
+                                    v-model="form.nameserver2"
+                                    placeholder="Masukkan Nameserver 2"
+                                    required
+                                    class="w-full !p-2.5 border dark:!border-typography-3 dark:!bg-dark-primary-2 rounded-lg dark:!text-white !text-sm"
+                                />
+                            </div>
+                            <div>
+                                <label class="block mb-1 text-start dark:!text-white text-sm"
+                                    >Tanggal Nameserver</label
+                                >
+                                <input
+                                    type="date"
+                                    v-model="form.tanggal_ns"
+                                    required
+                                    class="w-full !p-2.5 border dark:!border-typography-3 dark:!bg-dark-primary-2 rounded-lg dark:!text-white !text-sm"
+                                />
+                            </div>
+
+                            <div class="flex justify-end">
+                                <button
+                                    type="submit"
+                                    class="!bg-green-200 px-5 py-1 !text-sm !text-green-800 !font-medium border !border-green-800 rounded-lg"
+                                >
+                                    Simpan
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </template>
+    </v-dialog>
+</template>
---
+++ src/stores/pelanggan.js
@@ -0,0 +1,90 @@
+import { defineStore } from 'pinia'
+import api from '@/config/axios'
+import { useUIStore } from './ui'
+import { useResponseStore } from './response'
+
+export const usePelangganStore = defineStore('pelanggan', {
+    state: () => ({
+        datas: [],
+        data: {},
+    }),
+
+    actions: {
+        async getData() {
+            const uiStore = useUIStore()
+            uiStore.startLoading()
+            try {
+                const response = await api.get(`/pelanggan`)
+
+                this.datas = response.data.data
+            } catch (error) {
+                console.error('Gagal ambil data:', error)
+            } finally {
+                uiStore.stopLoading()
+            }
+        },
+
+        async addData(form) {
+            const uiStore = useUIStore()
+            uiStore.startLoading()
+
+            const responseStore = useResponseStore()
+            try {
+                const response = await api.post(`/name-servers`, form)
+                this.datas.push(response.data.data)
+
+                responseStore.addSuccess(response.data.message)
+            } catch (error) {
+                const errors = Object.values(error.response.data.errors)
+                errors.forEach((e) => {
+                    responseStore.addError(e)
+                })
+                console.error('Gagal simpan:', error)
+            } finally {
+                uiStore.stopLoading()
+            }
+        },
+        async editData(form, id) {
+            const uiStore = useUIStore()
+            uiStore.startLoading()
+
+            const responseStore = useResponseStore()
+            try {
+                const response = await api.put(`/name-servers/${id}`, form)
+                const updatedData = response.data.data
+                this.datas = this.datas.map((item) => {
+                    return item.id === Number(id) ? { ...item, ...updatedData } : item
+                })
+
+                responseStore.addSuccess(response.data.message)
+            } catch (error) {
+                const errors = Object.values(error.response.data.errors)
+                errors.forEach((e) => {
+                    responseStore.addError(e)
+                })
+                console.error('Gagal simpan:', error)
+            } finally {
+                uiStore.stopLoading()
+            }
+        },
+
+        async deleteData(id) {
+            const responseStore = useResponseStore()
+            const uiStore = useUIStore()
+            uiStore.startLoading()
+
+            try {
+                const response = await api.delete(`/name-servers/${id}`)
+
+                responseStore.addSuccess(response.data.message)
+                this.datas = this.datas.filter((ns) => {
+                    return ns.id !== id
+                })
+            } catch (error) {
+                console.error('Gagal hapus:', error)
+            } finally {
+                uiStore.stopLoading()
+            }
+        },
+    },
+})
